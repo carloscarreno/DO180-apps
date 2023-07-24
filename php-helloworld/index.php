505b5ad3cfbedc0d2001172355956e099de7b734
carloscarreno <student@workstation.lab.example.com>
2023-07-24 17:26:59 -0400

    Mi update

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print " From CodeReady Container 4.11 \n";
-print " git user carloscarreno \n";
+print " git user carloscarreno,24 Julio 2023 \n";
 ?>
